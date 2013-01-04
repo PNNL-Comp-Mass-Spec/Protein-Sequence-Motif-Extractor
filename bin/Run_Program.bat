c2b2552c823cbf9717a6ff70d182a47c3616ac40
Matthew Monroe <matthew.monroe@pnnl.gov>
2013-01-04 21:55:31 +0000

    Updated batch files

--- bin/Run_Program.bat
+++ bin/Run_Program.bat
@@ -1,5 +1,15 @@
+@echo off
 echo Find K# in proteins
+
+@echo on
 ProteinSequenceMotifExtractor.exe Example_Proteins.txt /M:K# /N:30 /K /T
 
+@echo off
 echo Use a RegEx to match DNA bases
+
+@echo on
 ProteinSequenceMotifExtractor.exe DNA.txt /M:"GTA.{8,10}TAC" /N:30 /K /T /X
+
+@echo off
+echo.
+pause
