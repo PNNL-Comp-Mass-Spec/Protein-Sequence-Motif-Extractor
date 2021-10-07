d34c33d6da6c4c4fb740702317a881bc8b795882
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-07 11:11:41 -0700

    Replace batch file

---
+++ ZippedForUpload/UpdateZipFile.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\7-Zip\7z.exe" a ProteinSequenceMotifExtractor_Program.zip ..\bin\PRISM.dll ..\bin\ProteinFileReader.dll ..\bin\ProteinSequenceMotifExtractor.exe ..\Readme.txt
+
+pause
