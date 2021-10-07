78b6312f749534a420d68892d8ff947dda0b3c7b
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-07 10:04:27 -0700

    Move options into new class

---
+++ FastaFileOptionsClass.vb
@@ -0,0 +1,30 @@
+﻿Public Class FastaFileOptionsClass
+
+    ''' <summary>
+    ''' Constructor
+    ''' </summary>
+    Public Sub New()
+        LookForAddnlRefInDescription = False
+        AddnlRefSepChar = "|"c
+        AddnlRefAccessionSepChar = ":"c
+    End Sub
+
+    ''' <summary>
+    ''' When true, look for additional references in the protein description
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property LookForAddnlRefInDescription As Boolean
+
+    ''' <summary>
+    ''' Additional reference separation character
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property AddnlRefSepChar As Char
+
+    ''' <summary>
+    ''' Additional reference accession separation character
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property AddnlRefAccessionSepChar As Char
+
+End Class
