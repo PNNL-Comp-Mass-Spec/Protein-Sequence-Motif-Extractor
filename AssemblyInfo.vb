8706d459447951fb90ea132596e6c54ee96d0678
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-06 18:44:47 -0700

    Move Assemblyinfo.vb up one directory and remove extra project files

---
+++ AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ProteinSequenceMotifExtractor")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("PNNL")>
+<Assembly: AssemblyProduct("ProteinSequenceMotifExtractor")>
+<Assembly: AssemblyCopyright("Copyright © PNNL 2010")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8c9d4d73-4d02-4512-b7b9-b795f8467b87")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.*")>
+
